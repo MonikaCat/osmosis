0f1c183dddab403f1e6514f97006378f2c22129c
Magic Cat <pusz.monika@gmail.com>
2022-01-25 13:44:30 +0000

    .

---
+++ x/lockup/types/expected_keepers.go
@@ -0,0 +1,22 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+)
+
+type AccountKeeper interface {
+	GetModuleAccount(ctx sdk.Context, moduleName string) authtypes.ModuleAccountI
+}
+
+// BankKeeper defines the expected interface needed to retrieve account balances.
+type BankKeeper interface {
+	GetAllBalances(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
+	GetBalance(ctx sdk.Context, addr sdk.AccAddress, denom string) sdk.Coin
+	LockedCoins(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
+	SpendableCoins(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
+
+	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
+	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+	BurnCoins(ctx sdk.Context, name string, amt sdk.Coins) error
+}
---
+++ x/txfees/keeper/feedecorator.go
@@ -0,0 +1,118 @@
+package keeper
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
+	"github.com/osmosis-labs/osmosis/x/txfees/keeper/txfee_filters"
+	"github.com/osmosis-labs/osmosis/x/txfees/types"
+)
+
+// DefaultArbMinGasFee if its not set in a config somewhere.
+// currently 0 uosmo/gas to preserve functionality with old node software
+// TODO: Bump after next minor version. (in 6.2+)
+var DefaultArbMinGasFee sdk.Dec = sdk.ZeroDec()
+
+// MempoolFeeDecorator will check if the transaction's fee is at least as large
+// as the local validator's minimum gasFee (defined in validator config).
+// If fee is too low, decorator returns error and tx is rejected from mempool.
+// Note this only applies when ctx.CheckTx = true
+// If fee is high enough or not CheckTx, then call next AnteHandler
+// CONTRACT: Tx must implement FeeTx to use MempoolFeeDecorator
+type MempoolFeeDecorator struct {
+	TxFeesKeeper       Keeper
+	ConfigArbMinGasFee sdk.Dec
+}
+
+func NewMempoolFeeDecorator(txFeesKeeper Keeper) MempoolFeeDecorator {
+	return MempoolFeeDecorator{
+		TxFeesKeeper:       txFeesKeeper,
+		ConfigArbMinGasFee: DefaultArbMinGasFee.Clone(),
+	}
+}
+
+func (mfd *MempoolFeeDecorator) SetArbMinGasFee(dec sdk.Dec) {
+	mfd.ConfigArbMinGasFee = dec
+}
+
+func (mfd MempoolFeeDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (newCtx sdk.Context, err error) {
+	// The SDK currently requires all txs to be FeeTx's in CheckTx, within its mempool fee decorator.
+	// See: https://github.com/cosmos/cosmos-sdk/blob/f726a2398a26bdaf71d78dbf56a82621e84fd098/x/auth/middleware/fee.go#L34-L37
+	// So this is not a real restriction at the moment.
+	feeTx, ok := tx.(sdk.FeeTx)
+	if !ok {
+		return ctx, sdkerrors.Wrap(sdkerrors.ErrTxDecode, "Tx must be a FeeTx")
+	}
+
+	feeCoins := feeTx.GetFee()
+
+	if len(feeCoins) > 1 {
+		return ctx, types.ErrTooManyFeeCoins
+	}
+
+	baseDenom, err := mfd.TxFeesKeeper.GetBaseDenom(ctx)
+	if err != nil {
+		return ctx, err
+	}
+
+	// If there is a fee attached to the tx, make sure the fee denom is a denom accepted by the chain
+	if len(feeCoins) == 1 {
+		feeDenom := feeCoins.GetDenomByIndex(0)
+		if feeDenom != baseDenom {
+			_, err := mfd.TxFeesKeeper.GetFeeToken(ctx, feeDenom)
+			if err != nil {
+				return ctx, err
+			}
+		}
+	}
+
+	// If we are in CheckTx, this function is ran locally to determine if these fees are sufficient
+	// to enter our mempool.
+	// So we ensure that the provided fees meet a minimum threshold for the validator,
+	// converting every non-osmo specified asset into an osmo-equivalent amount, to determine sufficiency.
+	if (ctx.IsCheckTx() || ctx.IsReCheckTx()) && !simulate {
+		minBaseGasPrice := mfd.GetMinBaseGasPriceForTx(ctx, baseDenom, tx)
+		if !(minBaseGasPrice.IsZero()) {
+			if len(feeCoins) != 1 {
+				return ctx, sdkerrors.Wrapf(sdkerrors.ErrInsufficientFee, "no fee attached")
+			}
+			err = mfd.TxFeesKeeper.IsSufficientFee(ctx, minBaseGasPrice, feeTx.GetGas(), feeCoins[0])
+			if err != nil {
+				return ctx, err
+			}
+		}
+	}
+
+	return next(ctx, tx, simulate)
+}
+
+func (k Keeper) IsSufficientFee(ctx sdk.Context, minBaseGasPrice sdk.Dec, gasRequested uint64, feeCoin sdk.Coin) error {
+	baseDenom, err := k.GetBaseDenom(ctx)
+	if err != nil {
+		return err
+	}
+
+	// Determine the required fees by multiplying the required minimum gas
+	// price by the gas limit, where fee = ceil(minGasPrice * gasLimit).
+	glDec := sdk.NewDec(int64(gasRequested))
+	requiredBaseFee := sdk.NewCoin(baseDenom, minBaseGasPrice.Mul(glDec).Ceil().RoundInt())
+
+	convertedFee, err := k.ConvertToBaseToken(ctx, feeCoin)
+	if err != nil {
+		return err
+	}
+	if !(convertedFee.IsGTE(requiredBaseFee)) {
+		return sdkerrors.Wrapf(sdkerrors.ErrInsufficientFee, "insufficient fees; got: %s which converts to %s. required: %s", feeCoin, convertedFee, requiredBaseFee)
+	}
+
+	return nil
+}
+
+func (mfd MempoolFeeDecorator) GetMinBaseGasPriceForTx(ctx sdk.Context, baseDenom string, tx sdk.Tx) sdk.Dec {
+	cfgMinGasPrice := ctx.MinGasPrices().AmountOf(baseDenom)
+
+	if txfee_filters.IsArbTxLoose(tx) {
+		return sdk.MaxDec(cfgMinGasPrice, mfd.ConfigArbMinGasFee)
+	}
+	return cfgMinGasPrice
+}
